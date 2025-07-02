96a509b7554c19bc31c1709090aa3b21ae20cf11
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2025-07-02 12:11:20 -0700

    Updated tests to use concurrency

--- runner/runner.go
+++ runner/runner.go
@@ -8,7 +8,25 @@ import (
 
 
 func RunAll() {
-	thermal := tests.ThermalTest{Sensor: mocks.MockThermalSensor{}}
-	result := thermal.Run()
-	fmt.Println(result)
+	fmt.Println("Running VoltCheck Tests (Concurrent)...")
+
+	// Channel to collect results
+	results := make(chan string, 2) 
+
+	// Launch ThermalTest inm a goroutine
+	go func() {
+		thermal := tests.ThermalTest{Sensor: mocks.MockThermalSensor{}}
+		results <- "ThermalTest: " + thermal.Run()
+	}()
+
+	// Launch VoltageTest in another goroutine
+	go func() {
+		voltage := tests.VoltageTest{Sensor: mocks.MockVoltageSensor{}}
+		results <- "VoltageTest: " + voltage.Run()
+	}()
+
+	// Collect and print results from both tests
+	for i := 0; i < 2; i++ {
+		fmt.Println(<-results)
+	}
 }
